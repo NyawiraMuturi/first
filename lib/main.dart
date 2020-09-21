2e0b792618feef2ea5b0388ce470954af5cc5103
Nyawira Muturi <albinamuturi@gmail.com>
2020-09-21 14:31:08 +0300

    Add image asset

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text("Hello World"),),),);
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://pixabay.com/images/search/nature/'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
